edb267d962dc300b6308ee69ea140d9b51230d90
donovan-herion <donovanherion@gmail.com>
2022-02-06 01:52:45 +0100

    finished migration didnt use cloud functions from firebase this time

--- helpers/axios.js
+++ helpers/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://amazon-clone-project-donovan.vercel.app",
   // "https://us-central1-amaclone-8c301.cloudfunctions.net/api",
   // "http://127.0.0.1:5001/amaclone-4e671/us-central1/api",
   // ancien "https://us-central1-amaclone-673b4.cloudfunctions.net/api",
