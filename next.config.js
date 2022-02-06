1732210c994dcf8557b8f69e1b1d6ee4132c263e
donovan-herion <donovanherion@gmail.com>
2022-02-06 01:51:26 +0100

    finished migration didnt use cloud functions from firebase this time

--- next.config.js
+++ next.config.js
@@ -2,4 +2,9 @@ module.exports = {
   images: {
     domains: ["images-na.ssl-images-amazon.com", "encrypted-tbn0.gstatic.com", "media.very.co.uk"],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
