c59dd5425c95f87ecf986c61dcba756a39edfa54
donovan-herion <donovanherion@gmail.com>
2022-02-06 02:08:57 +0100

    relaunch deployment

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ module.exports = {
     domains: ["images-na.ssl-images-amazon.com", "encrypted-tbn0.gstatic.com", "media.very.co.uk"],
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
